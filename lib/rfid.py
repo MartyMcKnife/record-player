496dc0b81acde1bcd2a27282ad8dd5bec6c64a0c
seanm <sean.mcdougall623@gmail.com>
2024-04-21 22:00:32 +0800

    tested and fixed rfid code to actually work properly
    the code is actually horrific so future me if you work out a better implementation please fix it

--- lib/rfid.py
+++ lib/rfid.py
@@ -1,4 +1,5 @@
-from pn532pi import Pn532
+from pn532pi import Pn532, Pn532I2c, pn532
+import time
 
 def readData(nfc: Pn532, uid: str) -> str:
     # Check if our NFC tag is the type we expect
@@ -6,28 +7,44 @@ def readData(nfc: Pn532, uid: str) -> str:
         # User data is stored between pages 4 and 215 in 4x2 bit chunks
         # We want everything from the last bit of page 5 onwards
         hexArray = []
-        page1Success, page1Data = nfc.mifareclassic_ReadPage(5)
-        
-        # Only run if we successfully grab the first page - if we don't, something is probably wrong
-        if page1Success:
-            ## last bit of page 5
-            hexArray.append(page1Data[-1:])
-            
-            run = True
-        
-            for page in range(6,216) and run:
-                success, data = nfc.mifareultralight_ReadPage(page)
+           
+        run = True
+    
+        for page in range(5,216):
+            if run:
+                success, raw = nfc.mifareultralight_ReadPage(page)
                 if success:
                     # loop through our page chunk
-                    for item in data:
-                        # fe is the 'final' bit - once we read it we have everything stored on the tag
-                        if item == "fe":
+                    for item in raw:
+                        # once we start reading '0' bits that means we have read all the available data on the rfid tag
+                        if item ==0:
                             run = False
                             break
                         else:
                             hexArray.append(item)
-                            
+                #tiny break to reset i2c device
+                time.sleep(0.0000001)
+            else:
+                break
+        # delete our last element in the array as they are a fake 'empty' bit
+        del hexArray[-1:]
+        # delete first 5 elements as the are meaningless data type points
+        del hexArray[:5]
+
         # Decode our full array and return it as a string
-        byteString = b''.join(hexArray)
-        return byteString.decode()
+        return ''.join(chr(i) for i in hexArray)
+
+
+# test code to make sure reader is working
+if __name__ == "__main__":
+    PN532_I2C = Pn532I2c(1)
+    nfc = Pn532(PN532_I2C)
+    nfc.begin()
+    nfc.SAMConfig()
+    while True:
+        print("waiting for card...")
+        success, uid = nfc.readPassiveTargetID(pn532.PN532_MIFARE_ISO14443A_106KBPS)
         
+        if success:
+            print(readData(nfc, uid))
+            break
