94bef8869d9701a15c1a1fa131a6ebdaa2afd23c
seanm <sean.mcdougall623@gmail.com>
2024-04-22 22:22:19 +0800

    wrote out code for generating centred qr code, created .gitignore

--- lib/lcd_screen.py
+++ lib/lcd_screen.py
@@ -1,12 +1,51 @@
-import qrcode
-from PIL import Image,ImageDraw,ImageFont
-from OLED_2in42 import OLED_2in42
+from luma.core.render import canvas
+from luma.oled.device import ssd1309, ssd1306
+from luma.core.interface.serial import i2c
+from PIL import Image
 
-def draw_qrcode(link: str, disp: OLED_2in42):
-    disp.clear()
-    image1 = Image.new("1", (disp.OLED_WIDTH, disp.OLED_HEIGHT), "WHITE")
-    
-    
-    disp.showImage(disp.getBuffer(image1))
-    
-    qrcode.make(link)
+import time
+import segno
+import io
+
+
+class display:
+    def __init__(self, device: ssd1309, px=8, py=8):
+        self.device = device
+        self.px = px
+        self.py = py
+
+    def draw_text(self, text: str, duration: int):
+        with canvas(self.device) as draw:
+            draw.text((30, 40), "Please scan QR Code to authenticate")
+            time.sleep(duration)
+
+    def draw_qrcode(self, link: str):
+        # generate our qr code
+        # this code keeps it in buffer, so we don't waste any file space
+        qrcode = segno.make(link)
+        out = io.BytesIO()
+        # makes the qr code have a dark background and white pixels
+        qrcode.save(out, kind="png", scale=4, dark="white", light=None)
+        out.seek(0)
+
+        # let pillow grab our qrcode
+        img = Image.open(out).resize((64, 64))
+
+        # create a display for our screen
+        screen = Image.new(self.device.mode, (128, 64))
+
+        # calcualte where our qr code should sit so it is centered
+        x = (screen.width - img.width) // 2
+        y = (screen.height - img.height) // 2
+
+        # put qr code on screen
+        screen.paste(img, (x, y))
+
+        # display!
+        device.display(screen)
+
+
+if __name__ == "__main__":
+    serial = i2c(port=1, address=0x3C)
+    device = ssd1306(serial)
+    display(device).draw_qrcode("https://www.google.com")
