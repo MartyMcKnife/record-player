878260434ff25ebad12bda693bcdd0e5fe40148f
seanm <sean.mcdougall623@gmail.com>
2024-05-16 20:24:31 +0800

    added test code to motor driver

--- lib/motor.py
+++ lib/motor.py
@@ -6,3 +6,8 @@ motor = RpiMotorLib.BYJMotor("1", "28BYJ")
 def drive_motor():
     while True:
         motor.motor_run([39, 33, 31, 29], wait=0.005)
+
+
+if __name__ == "__main__":
+    while True:
+        drive_motor()
