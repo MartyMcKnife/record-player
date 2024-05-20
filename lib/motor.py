01db445c0db44efb88b7e86593ae17b2e08d96f7
seanm <sean.mcdougall623@gmail.com>
2024-05-20 11:35:19 +0800

    cleaned up motor driver code to hopefully work

--- lib/motor.py
+++ lib/motor.py
@@ -1,13 +1,11 @@
 from RpiMotorLib import RpiMotorLib
 
-motor = RpiMotorLib.BYJMotor("1", "28BYJ")
+motor = RpiMotorLib.BYJMotor("motoror", "28BYJ")
 
 
 def drive_motor():
-    while True:
-        motor.motor_run([39, 33, 31, 29], wait=0.005)
+    motor.motor_run([37, 35, 33, 29], 0.01, 100, False, False, "half", 0.05)
 
 
 if __name__ == "__main__":
-    while True:
-        drive_motor()
+    drive_motor()
