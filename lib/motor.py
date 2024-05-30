929ced5c3b83d3e4d5a904227584c37d820bf880
seanm <sean.mcdougall623@gmail.com>
2024-05-30 21:03:43 +0800

    fixed motor running indef

--- lib/motor.py
+++ lib/motor.py
@@ -6,9 +6,12 @@ motor = RpiMotorLib.BYJMotor("motoror", "28BYJ")
 def drive_motor():
     # keep looping reallly quickly
     while True:
-        motor.motor_run(
-            [24, 23, 6, 5], 0.0005, 512000, False, False, "half", 0.05
-        )
+        try:
+            motor.motor_run(
+                [24, 23, 6, 5], 0.0005, 512000, False, False, "half", 0.05
+            )
+        except KeyboardInterrupt:
+            break
 
 
 if __name__ == "__main__":
